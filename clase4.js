a715bc4f36b49a3f07daccb08c5d0b2b23502e3f
Javier Campos <j_campos.v@outlook.com>
2020-08-12 18:48:00 -0400

    Clase 4 - Funciones

---
+++ clase4.js
@@ -0,0 +1,11 @@
+var nombre = 'Javier', edad = 23
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Darío', 27)
+imprimirEdad('Juan') //Salida: Juan tiene undefined años
