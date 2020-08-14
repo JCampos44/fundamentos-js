39b01ce36c9edc41820795fe64e28136a217438a
Javier Campos <j_campos.v@outlook.com>
2020-08-14 14:29:24 -0400

    Clase 9 - Comparaciones

---
+++ clase9.js
@@ -0,0 +1,14 @@
+/* Al compararlos con ==, retorna true, pero con === retorna false
+porque no son del mismo tipo */
+var x = 4, y = '4'
+
+
+/* Cualquier comparación entre estos dos objetos retorna false,
+ya que apuntan a dos posiciones distintas en memoria */
+var javier = {
+    nombre: 'Javier'
+}
+
+var otraPersona = {
+    nombre: 'Javier'
+}
